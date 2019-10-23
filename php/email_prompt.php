b4f07ea55072df72ebaff3c071dbc6ff4e345ade
jadonhansen <jadongavhansen@gmail.com>
2019-10-23 15:42:00 +0200

    :)

--- php/email_prompt.php
+++ php/email_prompt.php
@@ -3,21 +3,19 @@
 if (isset($_POST['submit'])) {
     $to = $_POST['email'];
     $subject = "ICON - Password Reset";
-    $link = "";
+    $link = "localhost:8080/Mamp/apache2/htdocs/Camagru/pages/forgettery.html";
     $message = "Use the following link to reset your password: ' $link '";
     $res = mail($to, $subject, $message);
     if ($res == TRUE) {
-        echo "<script>alert('Email sent!')</script>";
-        echo "<script>window.open('forgettery.html','_self')</script>";
+        echo "<script>alert('Email sent! We have sent you a password reset link.')</script>";
+        echo "<script>window.open('login.html','_self')</script>";
     }
     else {
         echo "<script>alert('Failed to send email!')</script>";
-        echo "<script>window.open('forgettery.html','_self')</script>";
+        echo "<script>window.open('login.html','_self')</script>";
     }
 }
-else {
-    echo "<script>alert('Welcome $username! Please login with your new details.')</script>";
+else
     echo "<script>window.open('login.html?','_self')</script>";
-}
 
 ?>
