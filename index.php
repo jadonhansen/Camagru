e65a59ab57c53c43bf000a90372c8909d9f5efbd
jadonhansen <jadongavhansen@gmail.com>
2019-10-23 12:15:10 +0200

    :)

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 session_start();
 
 if (isset($_SESSION['login']))
-	echo "<script>window.open('../pages/feed.html','_self')</script>";
+	echo "<script>window.open('./pages/feed.html','_self')</script>";
 else
-	echo "<script>window.open('../pages/login.html','_self')</script>";
+	echo "<script>window.open('./pages/login.html','_self')</script>";
 
 ?>
