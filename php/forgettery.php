d82d49e026e7dc6dd2cae827b81ed1a1905c6872
jadonhansen <jadongavhansen@gmail.com>
2019-10-28 19:14:35 +0200

    pass reset done

--- php/forgettery.php
+++ php/forgettery.php
@@ -5,35 +5,31 @@ if (isset($_POST['submit'])) {
     $username = $_POST['login'];
     $email = $_POST['email'];
     $new_password = $_POST['new_password'];
-    $repeat = $_POST['repeat'];
+	$repeat = $_POST['repeat'];
 
+	if (empty($new_password) || empty($username) || empty($email)) {
+		echo "<script>alert('Please fill in all fields!')</script>";
+		echo "<script>window.open('../pages/forgettery.php?error=emptyfields','_self')</script>";
+		exit();
+	}
 	if ($new_password !== $repeat) {
 		echo "<script>alert('Your passwords do not match! Please try again.')</script>";
 		echo "<script>window.open('../pages/forgettery.php?error=passwordsdifference','_self')</script>";
 		exit();		
 	}
-	if (empty($new_password) || empty($username) || empty($email)) {
-		echo "<script>window.open('../pages/forgettery.php?error=emptyfields','_self')</script>";
+	$new = hash("whirlpool", $new_password);
+	$stmt = $conn->prepare("UPDATE users SET passwd= :pass WHERE email= :eml AND username= :usrn");
+	$stmt->bindParam(':pass', $new);
+	$stmt->bindParam(':eml', $email);
+	$stmt->bindParam(':usrn', $username);
+	if (!$stmt->execute()) {
+		echo "<script>alert('SQL ERROR: 1')</script>";
+		echo "<script>window.open('../pages/forgettery.php?error=sqlerror','_self')</script>";
 		exit();
 	}
-	else {
-        $sql = "UPDATE users SET passwd=? WHERE email=? AND username=?";
-		$stmt = mysqli_stmt_init($conn);
-		if (!mysqli_stmt_prepare($stmt, $sql)) {
-			echo "<script>window.open('../pages/forgettery.php?error=sqlerror','_self')</script>";
-			exit();
-		}
-		else {
-            $hashed = $new_password;
-			mysqli_stmt_bind_param($stmt, "sss", $hashed, $email, $username);
-			mysqli_stmt_execute($stmt);
-			echo "<script>alert('Welcome $username! Please login with your new details.')</script>";
-			echo "<script>window.open('../pages/login.php','_self')</script>";
-				exit();
-		}
-        mysqli_stmt_close($stmt);
-        mysqli_close($conn);
-	}
+	echo "<script>alert('Saved! Please login with your new details as $username.')</script>";
+	echo "<script>window.open('../pages/login.php','_self')</script>";
+	$conn = NULL;
 }
 else {
     echo "<script>window.open('../pages/forgettery.php','_self')</script>";
