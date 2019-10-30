600f74a80b322cc51af21c695984ec07dd44fc29
jadonhansen <jadongavhansen@gmail.com>
2019-10-30 14:04:33 +0200

    validation

--- php/create.php
+++ php/create.php
@@ -32,7 +32,18 @@ function char_check($nam, $srnam, $usrnam, $eml) {
 		echo "<script>window.open('../pages/create.php?error=emptyfields','_self')</script>";
 		exit();
 	}
-	//check all normal characters
+	else if (!preg_match("/^[a-zA-Z0-9]*$/", $usrnam)) {
+		echo "<script>alert('Please make sure your username only consists of either uppercase letters, lowercase letters or number characters.')</script>";
+		echo "<script>window.open('../pages/create.php','_self')</script>";
+	}
+	else if (!preg_match("/^[a-zA-Z]*$/", $nam)) {
+		echo "<script>alert('Please make sure the name field only consists of uppercase or lowercase letters')</script>";
+		echo "<script>window.open('../pages/create.php','_self')</script>";
+	}
+	else if (!preg_match("/^[a-zA-Z]*$/", $surname)) {
+		echo "<script>alert('Please make sure the surname field only consists of uppercase or lowercase letters')</script>";
+		echo "<script>window.open('../pages/create.php','_self')</script>";
+	}
 }
 
 if (isset($_POST['submit'])) {
