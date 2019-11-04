b87c1281b066baeac09638c85a64c36e882a7181
Gabriel Strauss <gstrauss@c3r2s5.wethinkcode.co.za>
2019-11-04 17:44:02 +0200

    finished tidying pages

--- pages/login.php
+++ pages/login.php
@@ -14,79 +14,76 @@
       <div class="icon">
         ICON
       </div>
+      
       <!-- forgot password -->
       <a href="pass_reset.php" class="forgot">forgot password</a>
 
-    <!-- the differing logins and outs -->
-    <?php
-      session_start();
-      if(isset($_SESSION['username']))
-      {
-        echo "<form method='post' action='../php/logout.php' >";
-        echo"<button type='submit' id='logout' >Logout</button>";
-        echo "</form>";
-      }
-      else
-      { 
-        echo("
-        <div class='create'>
-          <h1><a id='link' href='create.php'>create account</a></h1>
-        </div>
-        ");
-      }
-    ?>
+      <!-- the differing logins and outs -->
+      <?php
+        session_start();
+        if(isset($_SESSION['username']))
+        {
+          echo "<form method='post' action='../php/logout.php' >";
+          echo"<button type='submit' id='logout' >Logout</button>";
+          echo "</form>";
+        }
+        else
+        { 
+          echo("<div class='create'>
+                  <h1><a id='link' href='create.php'>create account</a></h1>
+                </div>");
+        }
+      ?>
     </div>
 
-
     <!-- drop script -->
-<script>
-  function myFunction()
-  {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(event)
-  {
-    if (!event.target.matches('.dropbtn')) 
-    {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++)
+    <script>
+      function myFunction()
       {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show'))
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+  
+      window.onclick = function(event)
+      {
+        if (!event.target.matches('.dropbtn')) 
         {
-          openDropdown.classList.remove('show');
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++)
+          {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show'))
+            {
+              openDropdown.classList.remove('show');
+            }
+          }
         }
       }
-    }
-  }
-</script>
+    </script>
 
-<!-- this is the form box -->
-
-<div class="box">
-                <!-- the form -->
-                <div class="header">
-                    Login
-                </div>
-                <div class="info">
-                    <form action="../php/login.php" method="POST">
-                        <input type="text" name="username" class="field" placeholder="Username" onfocus="this.placeholder = ''" required>
-                        <input type="password" name="password" class="field" placeholder="Password" required>
-                        <input type="submit" name="submit" value="Create" class="form">
-                    </form>
-                </div>
+    <!-- this is the form box -->
+    <div class="box">            
+      <!-- the form -->
+      <div class="header">
+        Login
+      </div>
+      <div class="info">
+        <form action="../php/login.php" method="POST">
+          <input type="text" name="username" class="field" placeholder="Username" onfocus="this.placeholder = ''" required>
+          <input type="password" name="password" class="field" placeholder="Password" required>
+          <input type="submit" name="submit" value="Create" class="form">
+        </form>
+      </div>
                 
-                <!-- <hr class="the-line">   -->
+      <!-- <hr class="the-line">   -->
 
-                <!-- free browsing button -->
-                <button class="free" onclick="window.location.href = 'feed.php';">
-                    Guest Browse
-                </button>
-            </div>
+      <!-- free browsing button -->
+      <button class="free" onclick="window.location.href = 'feed.php';">
+        Guest Browse
+      </button>
+    </div>
 
- <!-- the foot -->
-        <div class="foot"></div>
-    </body>
+    <!-- the foot -->
+    <div class="foot"></div>
+  </body>
 </html>
--- pages/upload.php
+++ pages/upload.php
@@ -6,97 +6,91 @@
     <link rel="stylesheet" href="../css/body.css">
     <link rel="stylesheet" href="../css/head.css">
   </head>
-    <!-- background is the body id -->
-    <body id=bground>
-    
+
+  <body id=bground>    
     <div class="head">
 
-<!-- the drop down button -->
-<?php
-  session_start();
-  if(isset($_SESSION['username']))
-    echo("
-      <div class='dropdown'>
-      <button onclick='myFunction()' class='dropbtn'>
-      <div class='other_option'></div>
-      <div class='options'></div>
-      <div class='options'></div>
-      <div class='options'></div>
-      </button>
-      <div id='myDropdown' class='dropdown-content'>
-      <a href='profile.php'>Profile</a>
-      <a href='gallery.php'>Gallery</a>
-      <a href='feed.php'>Feed</a>
-      <a href='upload.php'>Upload</a>
+      <!-- the drop down button -->
+      <?php
+        session_start();
+        if(isset($_SESSION['username']))
+          echo("<div class='dropdown'>
+                  <button onclick='myFunction()' class='dropbtn'>
+                    <div class='other_option'></div>
+                    <div class='options'></div>
+                    <div class='options'></div>
+                    <div class='options'></div>
+                  </button>
+                  <div id='myDropdown' class='dropdown-content'>
+                    <a href='profile.php'>Profile</a>
+                    <a href='gallery.php'>Gallery</a>
+                    <a href='feed.php'>Feed</a>
+                    <a href='upload.php'>Upload</a>
+                  </div>
+                </div>");
+        else
+        {
+          echo "<script>alert('Please login first!')</script>";
+          echo "<script>window.open('./login.php','_self')</script>";
+        }
+      ?>
+    
+      <!-- Icon -->
+      <div class="icon">
+        ICON
       </div>
-      </div> 
-      ");
-  else {
-    echo "<script>alert('Please login first!')</script>";
-    echo "<script>window.open('./login.php','_self')</script>";
-  }
-?>
 
-  <!-- Icon -->
-  <div class="icon">
-    ICON
-  </div>
-
-
-    <!-- the differing logins and outs -->
-<?php
-  if(isset($_SESSION['username']))
-  {
-    echo("<a id='logout' href='../php/logout.php'>Logout</a>"); 
-  }
-  else
-  { 
-    echo("
-    <div class='create'>
-      <a id='link' href='create.php'>create account</a>
+      <!-- the differing logins and outs -->
+      <?php
+        if(isset($_SESSION['username']))
+        {
+          echo("<a id='logout' href='../php/logout.php'>Logout</a>"); 
+        }
+        else
+        { 
+          echo("<div class='create'>
+                  <a id='link' href='create.php'>create account</a>
+                </div>
+                <div class='create'>
+                  <a id='link' href='login.php'>Login</a>
+                </div>");
+        }
+      ?>
     </div>
-    <div class='create'>
-      <a id='link' href='login.php'>Login</a>
-    </div>
-    ");
-  }
-?>
-    </div>
-
-
 
     <!-- drop script -->
-<script>
-  function myFunction()
-  {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(event)
-  {
-    if (!event.target.matches('.dropbtn')) 
-    {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++)
+    <script>
+      function myFunction()
       {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show'))
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+  
+      window.onclick = function(event)
+      {
+        if (!event.target.matches('.dropbtn')) 
         {
-          openDropdown.classList.remove('show');
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++)
+          {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show'))
+            {
+              openDropdown.classList.remove('show');
+            }
+          }
         }
       }
-    }
-  }
-</script>
-<div class="pokemon">
-  <form action="../php/upload.php" method="post" enctype="multipart/form-data">
-    Upload from device:
-    <input type="file" name="uploadedFile" id="fileToUpload">
-    <input type="submit" name="uploadBtn" value="Upload Image">
-  </form>
-</div>
+    </script>
+      
+    <div class="pokemon">
+      <form action="../php/upload.php" method="post" enctype="multipart/form-data">
+        Upload from device:
+        <input type="file" name="uploadedFile" id="fileToUpload">
+        <input type="submit" name="uploadBtn" value="Upload Image">
+      </form>
+    </div>
 
-        <div class="foot"></div>
-    </body>
+    <div class="foot"></div>
+  </body>
 </html>
