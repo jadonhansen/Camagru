01d5ca875ca07ec8217f1f103b90ad3cbd7f323c
Gabriel Strauss <gstrauss@c3r2s5.wethinkcode.co.za>
2019-11-04 18:48:24 +0200

    worked on the feed page

--- pages/login.php
+++ pages/login.php
@@ -71,7 +71,7 @@
         <form action="../php/login.php" method="POST">
           <input type="text" name="username" class="field" placeholder="Username" onfocus="this.placeholder = ''" required>
           <input type="password" name="password" class="field" placeholder="Password" required>
-          <input type="submit" name="submit" value="Create" class="form">
+          <input type="submit" name="submit" value="Login" class="form">
         </form>
       </div>
                 
