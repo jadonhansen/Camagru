7de49590bc15fd3b8b966de2a19024a9c4609b20
jadonhansen <jadongavhansen@gmail.com>
2019-11-04 18:47:10 +0200

    post activity

--- php/db.php
+++ php/db.php
@@ -2,12 +2,12 @@
 
 $server = "localhost";
 $username = "root";
-$password = "qwerty";
+$password = "surfboard";
 
 try {
     $conn = new PDO("mysql:host=$server;dbname=icon", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<script>alert('Connected successfully')</script>";
+    // echo "<script>alert('Connected successfully')</script>";
 } 
 catch(PDOException $exception) {
     echo "Connection failed: " . $exception->getMessage(); //get this to echo an alert
