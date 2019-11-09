5151d02857db5ad7ee529fc2065ed7e838eccded
jadonhansen <jadongavhansen@gmail.com>
2019-11-09 22:24:51 +0200

    fixed footer

--- pages/create.php
+++ pages/create.php
@@ -1,12 +1,12 @@
 <html>
-  <head>
-    <title>ICON | Create Account</title>
-    <link rel="stylesheet" href="../css/dropdown.css">
-    <link rel="stylesheet" href="../css/footer.css">
-    <link rel="stylesheet" href="../css/body.css">
-    <link rel="stylesheet" href="../css/head.css">
-    <link rel="stylesheet" href="../css/block.css">
-  </head>
+	<head>
+		<title>ICON | Create Account</title>
+		<link rel="stylesheet" href="../css/dropdown.css">
+		<link rel="stylesheet" href="../css/footer.css">
+		<link rel="stylesheet" href="../css/body.css">
+		<link rel="stylesheet" href="../css/head.css">
+		<link rel="stylesheet" href="../css/block.css">
+	</head>
 
   <body class="bgcreate">
     <div class="head">
@@ -19,33 +19,27 @@
         session_start();
         if(isset($_SESSION['username']))
           echo("<a id='logout' href='../php/logout.php'>Logout</a>"); 
-        else 
-        {
+        else {
           echo("<div class='create'>
                   <a id='link' href='login.php'>Login</a>
                 </div>");
         }
       ?>
+
     </div>
 
     <!-- drop script -->
     <script>
-      function myFunction()
-      {
+      function myFunction() {
         document.getElementById("myDropdown").classList.toggle("show");
       }
-  
-      window.onclick = function(event)
-      {
-        if (!event.target.matches('.dropbtn')) 
-        {
+      window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
           var dropdowns = document.getElementsByClassName("dropdown-content");
           var i;
-          for (i = 0; i < dropdowns.length; i++)
-          {
+          for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show'))
-            {
+            if (openDropdown.classList.contains('show')) {
               openDropdown.classList.remove('show');
             }
           }
@@ -53,36 +47,35 @@
       }
     </script>
 
-    <!-- this is the form box -->
-    <div class="box">
-      <!-- the form -->
-      <div class="header">
-        Create Account
-      </div>
-      <div class="info">
-        <form action="../php/create.php" method="POST">
-          <input type="text" name="name" class="field" placeholder="Name" required>
-          <input type="text" name="surname" class="field" placeholder="Surname" required>
-          <input type="text" name="username" class="field" placeholder="Username" required>
-          <input type="email" name="email" class="field" placeholder="Email" required>
-          <input type="password" name="password" class="field" placeholder="Password" required>
-          <input type="submit" name="submit" value="Create" class="form">
-        </form>
-      </div>
+	<!-- this is the form box -->
+	<div class="box">
+	<!-- the form -->
+	<div class="header">
+		Create Account
+	</div>
+	<div class="info">
+		<form action="../php/create.php" method="POST">
+			<input type="text" name="name" class="field" placeholder="Name" required>
+			<input type="text" name="surname" class="field" placeholder="Surname" required>
+			<input type="text" name="username" class="field" placeholder="Username" required>
+			<input type="email" name="email" class="field" placeholder="Email" required>
+			<input type="password" name="password" class="field" placeholder="Password" required>
+			<input type="submit" name="submit" value="Create" class="form">
+		</form>
+	</div>
                 
       <!-- <hr class="the-line">   -->
 
-      <!-- free browsing button -->
-      <button class="free" onclick="window.location.href = 'feed.php';">
-        Guest Browse
-      </button>
-    </div>
+	<!-- free browsing button -->
+	<button class="free" onclick="window.location.href = 'feed.php';">Guest Browse</button>
+	</div>
 
-    <!-- the foot -->
-    <div class="foot">
-      <div class="jadon">@jhansen jadongavhansen@gmail.com</div> 
-      <div class="me">@gstrauss gstrauss18@gmail.com</div>
-      <div class="copyright">Copyright© 2019</div>
-    </div>
+	<!-- the foot -->
+	<div class="foot">
+		<div class="jadon">@jhansen jadongavhansen@gmail.com</div> 
+		<div class="me">@gstrauss gstrauss18@gmail.com</div>
+		<div class="copyright">Copyright© 2019</div>
+	</div>
+
   </body>
 </html>
--- pages/pass_reset.php
+++ pages/pass_reset.php
@@ -1,119 +1,109 @@
 <html>
-  <head>
-    <title>ICON | Password Reset</title>
-    <link rel="stylesheet" href="../css/dropdown.css">
-    <link rel="stylesheet" href="../css/footer.css">
-    <link rel="stylesheet" href="../css/block.css">
-    <link rel="stylesheet" href="../css/body.css">
-    <link rel="stylesheet" href="../css/head.css">
-  </head>
+	<head>
+		<title>ICON | Password Reset</title>
+		<link rel="stylesheet" href="../css/dropdown.css">
+		<link rel="stylesheet" href="../css/footer.css">
+		<link rel="stylesheet" href="../css/block.css">
+		<link rel="stylesheet" href="../css/body.css">
+		<link rel="stylesheet" href="../css/head.css">
+	</head>
 
-  <body class="bgforgot">    
+  <body class="bgforgot">
+
     <div class="head">
-      <!-- the drop down button -->    
-      <?php
-        session_start();
-        if(isset($_SESSION['username']))
-        {
-          echo("<div class='dropdown'>
-                  <button onclick='myFunction()' class='dropbtn'>
-                    <div class='other_option'></div>
-                    <div class='options'></div>
-                    <div class='options'></div>
-                    <div class='options'></div>
-                  </button>
-                  <div id='myDropdown' class='dropdown-content'>
-                    <a href='profile.php'>Profile</a>
-                    <a href='gallery.php'>Gallery</a>
-                    <a href='feed.php'>Feed</a>
-                    <a href='upload.php'>Upload</a>
-                  </div>
-                </div>");
-          
-          echo("<div class='loggedin-user'>@"
-                .$_SESSION['username']. 
-               "</div>");
-        }
-            
-      ?>
-      <!-- Icon -->
+
+<!-- the drop down button -->    
+<?php
+	session_start();
+	if(isset($_SESSION['username'])) {
+		echo("<div class='dropdown'>
+				<button onclick='myFunction()' class='dropbtn'>
+				<div class='other_option'></div>
+				<div class='options'></div>
+				<div class='options'></div>
+				<div class='options'></div>
+				</button>
+				<div id='myDropdown' class='dropdown-content'>
+				<a href='profile.php'>Profile</a>
+				<a href='gallery.php'>Gallery</a>
+				<a href='feed.php'>Feed</a>
+				<a href='upload.php'>Upload</a>
+				</div>
+			</div>");
+		
+		echo("<div class='loggedin-user'>@"
+			.$_SESSION['username']. 
+			"</div>");
+	}
+?>
+
+    <!-- Icon -->
     
-      <div class="icon">
-        ICON
-      </div>
+	<div class="icon">
+	ICON
+	</div>
 
-      <!-- the differing logins and outs -->
-      <?php
-        session_start();
-        if(isset($_SESSION['username']))
-        {
-          echo("<a id='logout' href='../php/logout.php'>Logout</a>"); 
-        }
-        else
-        { 
-          echo("<div class='create'>
-                  <a id='link' href='create.php'>create account</a>
-                </div>
-                <div class='create'>
-                  <a id='link' href='login.php'>Login</a>
-                </div>");
-        }
-      ?>
+	<!-- the differing logins and outs -->
+	<?php
+		session_start();
+		if(isset($_SESSION['username'])) {
+			echo("<a id='logout' href='../php/logout.php'>Logout</a>"); 
+		}
+		else {
+			echo("<div class='create'>
+					<a id='link' href='create.php'>create account</a>
+				</div>
+				<div class='create'>
+					<a id='link' href='login.php'>Login</a>
+				</div>");
+		}
+	?>
+
     </div>
 
+	<!-- drop script -->
+	<script>
+		function myFunction() {
+		document.getElementById("myDropdown").classList.toggle("show");
+		}
+		window.onclick = function(event) {
+			if (!event.target.matches('.dropbtn')) {
+				var dropdowns = document.getElementsByClassName("dropdown-content");
+				var i;
+				for (i = 0; i < dropdowns.length; i++) {
+					var openDropdown = dropdowns[i];
+					if (openDropdown.classList.contains('show')) {
+						openDropdown.classList.remove('show');
+					}
+				}
+			}
+		}
+	</script>
 
+	<!-- this is the form box -->
 
-    <!-- drop script -->
-    <script>
-      function myFunction()
-      {
-        document.getElementById("myDropdown").classList.toggle("show");
-      }
-  
-      window.onclick = function(event)
-      {
-        if (!event.target.matches('.dropbtn')) 
-        {
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++)
-          {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show'))
-            {
-              openDropdown.classList.remove('show');
-            }
-          }
-        }
-      }
-    </script>
+	<div class="box">
+		<!-- the form -->
+		<div class="header">Please enter your email:</div>
 
-    <!-- this is the form box -->
+		<div class="info">
+			<form action="../php/email_prompt.php" method="POST">
+				<input type="email" name="email" class="field" placeholder="Email" required>
+				<input type="submit" name="submit" value="Send" class="form">
+			</form>
+		</div>                
+		<!-- <hr class="the-line">   -->
+				
+		<!-- free browsing button -->
+		<button class="free" onclick="window.location.href = 'feed.php';">Guest Browse</button>
+	</div>
 
-    <div class="box">
-      <!-- the form -->
-      <div class="header">
-        Please enter your email:
-      </div>
-      <div class="info">
-        <form action="../php/email_prompt.php" method="POST">
-          <input type="email" name="email" class="field" placeholder="Email" required>
-          <input type="submit" name="submit" value="Send" class="form">
-        </form>
-      </div>                
-      <!-- <hr class="the-line">   -->
-                
-      <!-- free browsing button -->
-      <button class="free" onclick="window.location.href = 'feed.php';">
-        Guest Browse
-      </button>
-    </div>
+	<!-- the foot -->
+	<div class="foot">
+		<div class="jadon">@jhansen jadongavhansen@gmail.com</div> 
+		<div class="me">@gstrauss gstrauss18@gmail.com</div>
+		<div class="copyright">Copyright© 2019</div>
+	</div>
 
-    <!-- the foot -->
-    <div class="foot">
-      <div class="jadon">@jhansen jadongavhansen@gmail.com</div> 
-      <div class="me">@gstrauss gstrauss18@gmail.com</div>
-      <div class="copyright">Copyright© 2019</div>
-    </div>
   </body>
 </html>
